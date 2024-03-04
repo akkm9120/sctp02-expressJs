b78e88dfb9fd640f823e288e23b74db00f9621a8
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-03-04 05:41:31 +0000

    working on delete and update

--- 05-crud-handon/index.js
+++ 05-crud-handon/index.js
@@ -6,17 +6,41 @@ const hbs = require("hbs");
 
 app.set("view engine", "hbs");
 
-const databaase = [
-
-]
+const foodRecords = [
+    {
+        id: 1,
+        foodName: "Chicken Rice",
+        calories: 500,
+        meal:"lunch",
+        tags:["organic", "less-oil"]
+    },
+    {
+        id: 2,
+        foodName:"Boston Clam Chowder",
+        calories: 750,
+        meal:"dinner",
+        tags:["home-cooked"]
+    },
+    {
+        id: 3,
+        foodName:"Tuna Sandwich",
+        calories: 600,
+        meal:"snack",
+        tags:["gluten-free"]
+    }
+];
 
 app.get("/",function(req,res){
-
     res.render("index",{
-        'products' : databaase 
+        'foodRecords' : foodRecords 
     });
 })
 
+app.get("/delete-listing/:id",(req,res)=>{
+    res.render("delete-listing",{
+        
+    })
+})
 
 app.listen(3000,function (){
     console.log("Server has started...");
