522d6c9346b008e693e60dc4664ffdeb39bf7a55
Min <150890227+akkm9120@users.noreply.github.com>
2024-11-25 21:07:25 +0800

    updated routes

--- 05-crud-handon/index.js
+++ 05-crud-handon/index.js
@@ -1,93 +1,106 @@
 //steps "require" "route" "server"
 
 const express = require("express");
-const app = express ();
+const app = express();
 const hbs = require("hbs");
 
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: false }));
 
-const handlebarHelpers = require('handlebars-helpers')({
-    handlebars: hbs.handlebars
+const handlebarHelpers = require("handlebars-helpers")({
+  handlebars: hbs.handlebars,
 });
 
-
 const foodRecords = [
-    {
-        id: 1,
-        foodName: "Chicken Rice",
-        calories: 500,
-        meal:"lunch",
-        tags:["organic", "less-oil"]
-    },
-    {
-        id: 2,
-        foodName:"Boston Clam Chowder",
-        calories: 750,
-        meal:"dinner",
-        tags:["home-cooked"]
-    },
-    {
-        id: 3,
-        foodName:"Tuna Sandwich",
-        calories: 600,
-        meal:"snack",
-        tags:["gluten-free"]
-    }
+  {
+    id: 1,
+    foodName: "Chicken Rice",
+    calories: 500,
+    meal: "lunch",
+    tags: ["organic", "less-oil"],
+  },
+  {
+    id: 2,
+    foodName: "Boston Clam Chowder",
+    calories: 750,
+    meal: "dinner",
+    tags: ["home-cooked"],
+  },
+  {
+    id: 3,
+    foodName: "Tuna Sandwich",
+    calories: 600,
+    meal: "snack",
+    tags: ["gluten-free"],
+  },
 ];
 
-app.get("/",function(req,res){
-    res.render("index",{
-        'foodRecords' : foodRecords 
-    });
-})
+app.get("/", function (req, res) {
+  res.render("index", {
+    foodRecords: foodRecords,
+  });
+});
 
-app.get("/delete-listing/:id",(req,res)=>{
-    const idToDel = req.params.id;
-    const delRow = foodRecords.find(function(record){
-        return record.id == idToDel;
-    })
+app.get("/create-listing", (req, res) => {
+  res.render("create-listing");
+});
 
-    res.render("delete-listing",{
-        record: delRow
-    });
-})
+app.post("/create-listing", (req, res) => {
+  const newRecord = {
+    id: foodRecords.length + 1,
+    foodName: req.body.foodName,
+    calories: req.body.calories,
+    meal: req.body.meal,
+    tags: req.body.tags,
+  };
+  foodRecords.push(newRecord);
+  console.log(newRecord);
+  console.log(foodRecords);
+  res.redirect("/");
+});
 
+app.get("/delete-listing/:id", (req, res) => {
+  const idToDel = req.params.id;
+  const delRow = foodRecords.find(function (record) {
+    return record.id == idToDel;
+  });
 
-app.post("/delete-listing/:id",(req,res)=>{
-    const id = req.params.id ;
-    
-    const indexToDelete = foodRecords.findIndex(function(record){
-        return record.id == id;
-    })
+  res.render("delete-listing", {
+    record: delRow,
+  });
+});
 
-    foodRecords.splice(indexToDelete,1);
-   res.redirect("/");
-})
+app.post("/delete-listing/:id", (req, res) => {
+  const id = req.params.id;
 
+  const indexToDelete = foodRecords.findIndex(function (record) {
+    return record.id == id;
+  });
 
-app.get("/update/:item",(req,res)=>{
-    const id = req.params.item;
+  foodRecords.splice(indexToDelete, 1);
+  res.redirect("/");
+});
 
-    const updateItem = foodRecords.find(function(record){
-        return record.id == id;
-    })
+app.get("/update/:item", (req, res) => {
+  const id = req.params.item;
 
+  const updateItem = foodRecords.find(function (record) {
+    return record.id == id;
+  });
 
-    console.log(updateItem)
-    res.render("update",{
-        record : updateItem
-    })
-})
+  console.log(updateItem);
+  res.render("update", {
+    record: updateItem,
+  });
+});
 
-app.post("/update/:item",(req,res)=>{
-    const id = req.params.item;
-    const indexUpdate = foodRecords.findIndex(function(record){
-        return record.id == id;
-    })
+app.post("/update/:item", (req, res) => {
+  const id = req.params.item;
+  const indexUpdate = foodRecords.findIndex(function (record) {
+    return record.id == id;
+  });
+});
 
-})
-
-app.listen(3000,function (){
-    console.log("Server has started...");
-})
+app.listen(3000, function () {
+  console.log("Server has started...");
+});
